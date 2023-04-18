f5adbf98c09917ca01fb6ede8a49158aac5375e5
Jacob Poulsgaard Tjørnholm <jacob@tjoernholm.dk>
2023-04-19 00:33:36 +0200

    Rewrite to traverse the code

--- src/plait/core.clj
+++ src/plait/core.clj
@@ -25,15 +25,21 @@
                          [n (get new-bindings-m n b)]))
                 (remove (comp base-bindings-s first) new-binding-pairs)))))
 
-(def ^:dynamic *plait-bindings* [])
+(defn plait-impl [bindings x]
+  (if (and (seqable? x)
+           (= 'plait (first x)))
+    (let [child-bindings (rename-underscore-bindings (second x))
+          child-body (drop 2 x)
+          new-bindings (merge-bindings bindings child-bindings)]
+      (plait-impl new-bindings
+                  (concat ['let new-bindings]
+                          (map (partial plait-impl new-bindings) child-body))))
+    x))
 
 (defmacro plait
   "Add `let` style bindings that can be redeclared at deeper levels."
   {:style/indent 1}
   [bindings & body]
-  (let [merged-bindings (merge-bindings *plait-bindings*
-                                        (rename-underscore-bindings bindings))]
-    (binding [*plait-bindings* merged-bindings]
-      (w/macroexpand-all
-       `(let ~merged-bindings
-          ~@body)))))
+  (let [top-level-bindings (rename-underscore-bindings bindings)
+        top-level-form (concat ['plait top-level-bindings] body)]
+    (plait-impl top-level-bindings top-level-form)))
